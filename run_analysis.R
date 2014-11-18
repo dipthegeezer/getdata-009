0c9a9756fd1a29de70f2d4c03245f0cc1235304b
Dipesh Patel <dipthegeezer@opensource.gmail.com>
2014-11-18 17:56:00 +0000

    Inital commit of first few parts.

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+library(dplyr)
+
+
+load_set <- function(path, type){
+    X <- read.table(file.path(path, type, paste("X_", type, ".txt", sep = "")), sep = "" , header = F, stringsAsFactors = F)
+    Y <- read.table(file.path(path, type, paste("y_", type, ".txt", sep = "")), sep = "" , header = F, stringsAsFactors = F)
+    subject <- read.table(file.path(path, type, paste("subject_", type, ".txt", sep = "")), sep = "" , header = F, stringsAsFactors = F)
+    data <- cbind(cbind(subject,Y), X)
+    rm(X, Y, subject)
+    return(data)
+}
+
+
+load_files <- function(path){
+    headers <- read.table(file.path(path, "features.txt"), sep = "" , header = F, stringsAsFactors = F)
+    headers <- rbind(c("subject"),rbind(c("activity"), headers))
+    headers <- headers[,c("V2")]
+    
+    test <- load_set(path, "test")
+    colnames(test) <- headers
+
+    train <- load_set(path, "train")
+    colnames(train) <- headers
+    
+    data <- rbind(test, train)
+    data <- data[grep("subject|activity|mean\\(\\)|std\\(\\)", headers, perl=TRUE, value=FALSE)]
+    rm(test, train, headers)
+    return(data)
+}
+
+
+
+
+
